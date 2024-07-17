70f7496761655ef410acd7a6491afd5e79fe0e06
Puchana <puchana123@gmail.com>
2024-07-17 15:43:02 +0700

    feat: add get route /api/books/:id

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,7 @@ mongoose.connect(url);
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(async function (req, res){
+    .get(async function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
       // query all books
@@ -28,12 +28,12 @@ module.exports = function (app) {
       res.json(data);
       console.log('get all books');
     })
-    
-    .post(async function (req, res){
+
+    .post(async function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
       // check title input
-      if(!title){
+      if (!title) {
         res.send('missing required field title');
         return;
       };
@@ -54,28 +54,39 @@ module.exports = function (app) {
         console.log('can not add new book:', error);
       }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try {
+        const exist_book = await Book.findById(bookid);
+        // check exist book
+        if (!exist_book) {
+          res.send('no book exists');
+          return;
+        };
+        res.json(exist_book);
+      } catch (error) {
+        console.log('can not find book:', error);
+      }
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
+
 };
